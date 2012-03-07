ed4489bd574893b39ec3823b7b09af876fce1c63
Leo Sudarma <lionstar_java@yahoo.com>
2012-03-07 16:00:58 -0500

    leo 1st commit

---
+++ leo/src/leo/Test.java
@@ -0,0 +1,14 @@
+package leo;
+
+public class Test {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Leo Test1");
+
+	}
+
+}
