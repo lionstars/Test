e65c3a5fafb020e6946be874a3c819f60c5f5f7f
Leo Sudarma <lionstar_java@yahoo.com>
2012-03-08 22:10:30 -0500

    commit on 8 march

--- leo/src/leo/Test.java
+++ leo/src/leo/Test.java
@@ -8,6 +8,7 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Leo Test1");
+		System.out.println("Test 2 on  March 8 , 2012");
 
 	}
 
